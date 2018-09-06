2cb2add83d59fa5c5cc83f6d6c5f6bd7b26fdc0b
Rahul <mail.rgupta25@gmail.com>
2018-09-07 01:35:35 +0530

    Phase 1: services implemented for right section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,23 @@ import { WeatherDataService } from './weather-data.service';
 })
 export class AppComponent {
   title = 'weather-app';
-
   forecastForCurrentCity: any;
+  fiveDayForecast: any;
+  sixteenDayForecast: any;
+  currentTab: any = 'Today';
+
+
   constructor(private weatherService: WeatherDataService){
     this.weatherService.getForecastForCurrentCity().subscribe(res =>{
       this.forecastForCurrentCity = res;
+    });
+
+    this.weatherService.getForecastForFiveDays().subscribe(res =>{
+      this.fiveDayForecast = res;
+    });
+
+    this.weatherService.getForecastForSixteenDays().subscribe(res => {
+      this.sixteenDayForecast =res;
     });
   }
 
@@ -21,12 +33,15 @@ export class AppComponent {
     console.log('Date :' + date);
     let time = new Date(date);
     console.log('Time :' + time);
-    return time.toLocaleString('en-US', { timeZone: 'Asia/Kolkata',month: 'short', day: 'numeric',year: 'numeric' });
-    //return time.toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
-    //return time;
+    return time.toLocaleString('en-US', { timeZone: 'Asia/Kolkata',month: 'short', day: 'numeric',year: 'numeric' });    
   }
 
   getTemperature(temp){
     return Math.trunc(temp);
   }
+
+  selectTab(tabValue){
+    this.currentTab = tabValue;
+  }
+
 }
--- src/app/weather-data.service.ts
+++ src/app/weather-data.service.ts
@@ -12,10 +12,22 @@ export class WeatherDataService {
    }
 
    getForecastForCurrentCity(): Observable<any>{
-     let url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=048c43a2f7e00f37c3b4044df2ec3128";
+    let url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=048c43a2f7e00f37c3b4044df2ec3128";
      return this.http.get(url);
    }
 
-   
+   getForecastForFiveDays():Observable<any>{
+    let url = "http://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&appid=048c43a2f7e00f37c3b4044df2ec3128";
+    return this.http.get(url);
+   }
+
+   getForecastForSixteenDays():Observable<any>{
+     let url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=16&appid=048c43a2f7e00f37c3b4044df2ec3128";
+     return this.http.get(url);
+   }
+
+
+
+
 
 }
